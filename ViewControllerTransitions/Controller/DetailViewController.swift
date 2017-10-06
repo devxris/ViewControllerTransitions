94d0a37727cb25f6eb3385830715f0d2b47437a2
devxris <chuwei0915@gmail.com>
2017-10-06 15:30:59 +0800

    implement default slide up transition (present modally segue)

---
+++ ViewControllerTransitions/Controller/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  ViewControllerTransitions
+//
+//  Created by Chris Huang on 06/10/2017.
+//  Copyright © 2017 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+	@IBAction func close(_ sender: UIButton) {
+		dismiss(animated: true, completion: nil)
+	}
+}
